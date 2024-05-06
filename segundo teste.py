84fc526ca7b6b4de93193463fc876a7407b80dea
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-06 16:22:29 -0300

    finalizei meu segundo teste .

---
+++ segundo teste.py
@@ -0,0 +1,12 @@
+def alo(horario)
+    print("Alo pessoal")
+    if horario >=6 and horario<=12:
+        print("Bom dia")
+    elif horario>12 and horario <=18
+        print("Boa tarde")
+    else:
+        print ("Boa noite")
+
+
+alo (10)
+alo (22)
