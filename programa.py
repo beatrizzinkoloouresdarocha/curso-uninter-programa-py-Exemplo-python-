9d2d2d1476a093388554f6de202bedd558d41183
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-06 16:01:10 -0300

    testei o mesmo que ele fez deu certo funcionou.

--- programa.py
+++ programa.py
@@ -1,17 +1,6 @@
-import subprocess
+def imprimir():
+    print("Alo mamae")
+    print("Alo papai")
 
-def executar_comando(comando):
-    try:
-        # Executa o comando e captura a saída
-        saida = subprocess.check_output(comando, shell=True, stderr=subprocess.STDOUT)
-        # Decodifica a saída para string
-        saida_str = saida.decode("utf-8")
-        return saida_str
-    except subprocess.CalledProcessError as e:
-        # Se ocorrer um erro, imprime a mensagem de erro
-        print("Erro ao executar o comando:", e.output)
-        return None
-
-# Exemplo de uso:
-resultado = executar_comando("ls -l")
-print(resultado)
+imprimir()
+imprimir()
